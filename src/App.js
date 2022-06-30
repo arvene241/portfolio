01a64344d1e2a65988d31b41a2dc3162ad174e1e
arvene <arvene241@gmail.com>
2022-06-30 20:16:51 +0800

    remove preloader

--- src/App.js
+++ src/App.js
@@ -2,38 +2,12 @@ import React, { useState, useEffect } from "react";
 import ThemeContextProvider from "./context/ThemeModeContext";
 import Homepage from "./pages/Homepage";
 import GlobalStyle from "./styles/GlobalStyle";
-import { AnimatePresence, motion } from "framer-motion";
-import Preloader from './components/Preloader';
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoaded(true);
-    }, 2000);
-  }, [isLoaded]);
-
   return (
     <ThemeContextProvider>
       <GlobalStyle />
-      <AnimatePresence>
-        {!isLoaded && (
-          <motion.div
-            exit={{
-              opacity: 0,
-              transition: {
-                ease: "easeInOut",
-                duration: 0.2,
-              },
-            }}
-          >
-            <Preloader />
-          </motion.div>
-        )}
-      </AnimatePresence>
-
-      {isLoaded && <Homepage />}
+      <Homepage />
     </ThemeContextProvider>
   );
 }
